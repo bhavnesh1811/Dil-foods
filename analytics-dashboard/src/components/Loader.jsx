776aebb0ce8599cce9ef1bc5e12e8ef61d53f67a
bhavnesh1811 <erbhavnesharora@gmail.com>
2024-02-21 02:22:28 +0530

    Loader updated

--- analytics-dashboard/src/components/Loader.jsx
+++ analytics-dashboard/src/components/Loader.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Stack, Skeleton } from "@chakra-ui/react";
+import React from "react";
 
 const Loader = () => {
   return (
-    <div>Loader....</div>
-  )
-}
+    <Stack>
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+      <Skeleton height={"50px"} />
+    </Stack>
+  );
+};
 
-export default Loader
+export default Loader;
